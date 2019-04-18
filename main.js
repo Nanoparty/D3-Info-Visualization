813c122cee9ddbc8f78a51b3bbd5d75505055a32
John Landers <john.landers@gatech.edu>
2019-04-18 18:05:22 -0400

    2D dimension reduction scatterplot
    swiggity swag

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-var width =1000;
-var height= 500;
+var width=1000;
+var height=600;
+var xaxis_height=500;
 
 d3.csv("movies.csv", function(csv) {
 	csv = csv.filter(function(d) {
@@ -64,20 +65,19 @@ d3.csv("movies.csv", function(csv) {
     // var title_year_Extent = d3.extent(csv, function(d) { return d.title_year; });
     // var budget_Extent = d3.extent(csv,  function(d) { return d.budget;  });
     // var gross_Extent = d3.extent(csv,  function(d) {return d.gross;   });
-    var duration_Extent = d3.extent(csv,  function(d) {return d.duration;   });
-    //console.log(duration_Extent);
+    // var duration_Extent = d3.extent(csv,  function(d) {return d.duration;   });
+    // console.log(duration_Extent);
 
-    var function_list = [];
+    var xfunction_list = [];
+    var yfunction_list = [];
 
 
     // Axis setup
-    var xScale = d3.scaleLinear().domain([0, 1]).range([50, 800]);
-    //this xscale is not used bc it is written over in createAxis(), but needs to be here do keep d3 from complaining
-    var yScale = d3.scaleLinear().domain([0, 1]).range([470, 30]);
+    var xScale = d3.scaleLinear().domain([0, 1]).range([50, 500]);
+    var yScale = d3.scaleLinear().domain([0, 1]).range([500, 50]);
 
 
     var xAxis = d3.axisBottom().scale(xScale);
-    //this xaxis is not used bc it is written over in createAxis(), but needs to be here do keep d3 from complaining
     var yAxis = d3.axisLeft().scale(yScale);
 
     //Create chart1G where the the attribute boxes will go
@@ -85,49 +85,64 @@ d3.csv("movies.csv", function(csv) {
         .append('p')
         .append('button')
         .style("border", "1px solid black")
-        .text('Add Attribute')
+        .text('Add X Attribute')
         .on('click', function() {
-            if(numAtts < 5){
-                numAtts++;
-                addAttributes();
+            if(xnumAtts < 5){
+                xnumAtts++;
+                addXAttributes();
             }
         });
 
-    var numAtts=1;
-    function_list.push(parameters[0]); //make duration the first attribute of the page
-    addAttributes(); //initial call of add attributes to make duration the initial attribute with a weight of 1
+    chart2G = d3.select("#chart2")
+        .append('p')
+        .append('button')
+        .style("border", "1px solid black")
+        .text('Add Y Attribute')
+        .on('click', function() {
+            if(ynumAtts < 5){
+                ynumAtts++;
+                addYAttributes();
+            }
+        });
 
-    function addAttributes() {
+    var xnumAtts=1;
+    var ynumAtts=1;
+    xfunction_list.push(parameters[0]); //make duration the first attribute of the page
+    yfunction_list.push(parameters[0]); //make duration the first attribute of the page
+    addXAttributes(); //initial call of add attributes to make duration the initial attribute with a weight of 1
+    addYAttributes();
+
+    function addXAttributes() {
         chart1G.selectAll("select").remove();
 
         var data = ["duration", "imdb_score", "title_year","gross","budget"];
 
-        //numAtts++;
+        //xnumAtts++;
 
         d3.select('#chart1')
             .append('input')
             .attr('type','text')
-            .attr('name','weight'+numAtts)
-            .attr('class','input'+numAtts)
-            .attr('id',function(d,i){return 'weight'+numAtts;})
+            .attr('name','xweight'+xnumAtts)
+            .attr('class','xinput'+xnumAtts)
+            .attr('id',function(d,i){return 'xweight'+xnumAtts;})
             .attr('value','1')
             .on('change',function(d,i){
                 selectValue = d3.select(this).property('value')
                 console.log(selectValue)
-                selectValue2 = d3.select('#select'+i).property('value')
+                selectValue2 = d3.select('#xselect'+i).property('value')
                 console.log(selectValue2) 
             });
 
         var select = d3.select('#chart1')
 			            .append('select')
-			            .attr('class','select'+numAtts)
-			            .attr('id',function(d,i){return 'select'+numAtts;})
+			            .attr('class','xselect'+xnumAtts)
+			            .attr('id',function(d,i){return 'xselect'+xnumAtts;})
 			            .on('change',function(d,i) {
 			                // selectValue = d3.select(this).property('value');
 			                // console.log(selectValue);
 			             	// change dropdown
-			                updateScale();
-			                updateAxis();
+			                updateXScale();
+			                updateXAxis();
 			                updateDots();
 			            });
 
@@ -137,19 +152,59 @@ d3.csv("movies.csv", function(csv) {
 			            .text(function (d) { return d; });
     }
 
-    //Create chart2G which is going to hold the scatterplot
-    var chart2G = d3.select("#chart2")
+    function addYAttributes() {
+        chart2G.selectAll("select").remove();
+
+        var data = ["duration", "imdb_score", "title_year","gross","budget"];
+
+        //xnumAtts++;
+
+        d3.select('#chart2')
+            .append('input')
+            .attr('type','text')
+            .attr('name','yweight'+ynumAtts)
+            .attr('class','yinput'+ynumAtts)
+            .attr('id',function(d,i){return 'yweight'+ynumAtts;})
+            .attr('value','1')
+            .on('change',function(d,i){
+                selectValue = d3.select(this).property('value')
+                console.log(selectValue)
+                selectValue2 = d3.select('#yselect'+i).property('value')
+                console.log(selectValue2) 
+            });
+
+        var select = d3.select('#chart2')
+			            .append('select')
+			            .attr('class','yselect'+ynumAtts)
+			            .attr('id',function(d,i){return 'yselect'+ynumAtts;})
+			            .on('change',function(d,i) {
+			                // selectValue = d3.select(this).property('value');
+			                // console.log(selectValue);
+			             	// change dropdown
+			                updateYScale();
+			                updateYAxis();
+			                updateDots();
+			            });
+
+        var options = select.selectAll('option')
+			            .data(data).enter()
+			            .append('option')
+			            .text(function (d) { return d; });
+    }
+
+    //Create chart3G which is going to hold the scatterplot
+    var chart3G = d3.select("#chart3")
 	                .append("svg:svg")
 	                .attr("width",width)
 	                .attr("height",height)
                     .append('g');
 
-//These are formatting functions for displaying info in chart3, but it's actually updated in the "on click" function
+	//These are formatting functions for displaying info in chart4, but it's actually updated in the "on click" function
     var formatComma = d3.format(","), 
     	formatDecimal = d3.format(".1f");
 
     //populate the graph based on this function
-    var temp2 = chart2G.selectAll(".dot")
+    var temp3 = chart3G.selectAll(".dot")
                     .data(csv)
                     .enter()
                     .append("circle")
@@ -158,11 +213,19 @@ d3.csv("movies.csv", function(csv) {
                     .attr("stroke", "black")
                     .attr("cx", function(d) {
                     	var score = 0;
-                    	for (var i = 0; i < function_list.length; i++) {
-                    		score += (d[function_list[i].id]-function_list[i].min)/(function_list[i].max-function_list[i].min)
+                    	for (var i = 0; i < xfunction_list.length; i++) {
+                    		score += (d[xfunction_list[i].id]-xfunction_list[i].min)/(xfunction_list[i].max-xfunction_list[i].min)
                     	}
-                    	return xScale(score); })
-                    .attr("cy", height - 30.0-100)
+                    	return xScale(score); 
+                    })
+                    // .attr("cy", xaxis_height)
+                    .attr("cy",function(d) {
+                    	var score = 0;
+                    	for (var i = 0; i < yfunction_list.length; i++) {
+                    		score += (d[yfunction_list[i].id]-yfunction_list[i].min)/(yfunction_list[i].max-yfunction_list[i].min)
+                    	}
+                    	return yScale(score); 
+			        })
                     .attr("r", 5)
                     .on("click", function(d,i)
                     {
@@ -181,39 +244,46 @@ d3.csv("movies.csv", function(csv) {
                         d3.select('#year').text(d.title_year);
                     });
 
-    //These are formatting functions for displaying info in chart3
+    //These are formatting functions for displaying info in chart4
     var formatComma = d3.format(","), 
     	formatDecimal = d3.format(".1f");
 
     // Draw the axis and labels the first time
 
 
-    chart2G.append("g") // create a group node
-			.attr("transform", "translate(0,"+ (450)+ ")")
-			.attr("class", "x axis")
-			.call(xAxis)
-			.append("text")
-			.attr("class", "label")
-			.attr("x", width-200)
-			.attr("y", -6)
-			.style("text-anchor", "end")
-			.text("High Favorability")
-			.style("fill", "black");
+    chart3G.append("g") // create a group node
+		.attr("transform", "translate(0,"+xaxis_height+ ")")
+		.attr("class", "x axis")
+		.call(xAxis)
 
-    chart2G.append("g") // create a group node
-		.attr("transform", "translate(0,"+ (450)+ ")")
+	chart3G.append("g") // create a group node
+		.attr("transform", "translate(50, 0)")
+		.attr("class", "y axis")
+		.call(yAxis)
+
+	chart3G.append("text")
+	.attr("transform", "translate(0,"+xaxis_height+ ")")
+		.attr("class", "label")
+		.attr("x", 500)
+		.attr("y", 30)
+		.style("text-anchor", "end")
+		.text("High Favorability")
+		.style("fill", "black");
+
+    chart3G.append("g") // create a group node
+		.attr("transform", "translate(0,"+xaxis_height+ ")")
 		//.call(xAxis)
 		.append("text")
 		.attr("class", "label")
-		.attr("x", 150)
-		.attr("y", -6)
+		.attr("x", 140)
+		.attr("y", 30)
 		.style("text-anchor", "end")
 		.text("Low Favorability")
 		.style("fill", "black");
 
-    chart2G.append("text")
+    chart3G.append("text")
         .attr("x", (400))
-        .attr("y", (100))
+        .attr("y", (25))
         .attr("text-anchor", "middle")
         .style("font-size", "16px")
         .style("text-decoration", "underline")
@@ -222,45 +292,84 @@ d3.csv("movies.csv", function(csv) {
         
 
 
-    function updateScale() {
-    	function_list = []; //clear the function_list because we're about to read it from the webpage html
+    function updateXScale() {
+    	xfunction_list = []; //clear the xfunction_list because we're about to read it from the webpage html
 
-    	for (var i = 1; i <= numAtts; i++) {
-    		var selectValue = d3.select('#select' + i).property('value');
+    	for (var i = 1; i <= xnumAtts; i++) {
+    		var selectValue = d3.select('#xselect' + i).property('value');
     		//console.log(selectValue);
     		if (selectValue === parameters[0].id) {
-        		function_list.push(parameters[0]);
+        		xfunction_list.push(parameters[0]);
         	} else if (selectValue === parameters[1].id) {
-        		function_list.push(parameters[1]);
+        		xfunction_list.push(parameters[1]);
         	} else if (selectValue === parameters[2].id) {
-        		function_list.push(parameters[2]);
+        		xfunction_list.push(parameters[2]);
         	} else if (selectValue === parameters[3].id) {
-        		function_list.push(parameters[3]);
+        		xfunction_list.push(parameters[3]);
         	} else if (selectValue === parameters[4].id) {
-        		function_list.push(parameters[4]);
+        		xfunction_list.push(parameters[4]);
         	}
     	}
-    	console.log("Updated function_list in updateScale():");
-    	console.log(function_list);
+    	console.log("Updated xfunction_list in updateXScale():");
+    	console.log(xfunction_list);
 
-    	xScale = d3.scaleLinear().domain([0, numAtts]).range([50, 800]);//numAtts should reflect the number of attributeson the page
+    	xScale = d3.scaleLinear().domain([0, xnumAtts]).range([50, 500]);//xnumAtts should reflect the number of xattributes on the page
     	xAxis = d3.axisBottom().scale(xScale);
     }
 
-    function updateAxis() {
-    	chart2G.select(".x.axis")
+    function updateYScale() {
+    	yfunction_list = []; //clear the yfunction_list because we're about to read it from the webpage html
+
+    	for (var i = 1; i <= ynumAtts; i++) {
+    		var selectValue = d3.select('#yselect' + i).property('value');
+    		//console.log(selectValue);
+    		if (selectValue === parameters[0].id) {
+        		yfunction_list.push(parameters[0]);
+        	} else if (selectValue === parameters[1].id) {
+        		yfunction_list.push(parameters[1]);
+        	} else if (selectValue === parameters[2].id) {
+        		yfunction_list.push(parameters[2]);
+        	} else if (selectValue === parameters[3].id) {
+        		yfunction_list.push(parameters[3]);
+        	} else if (selectValue === parameters[4].id) {
+        		yfunction_list.push(parameters[4]);
+        	}
+    	}
+    	console.log("Updated yfunction_list in updatYScale():");
+    	console.log(yfunction_list);
+
+    	yScale = d3.scaleLinear().domain([0, ynumAtts]).range([500, 50]);//ynumAtts should reflect the number of yattributes on the page
+    	yAxis = d3.axisLeft().scale(yScale);
+    }
+
+    function updateXAxis() {
+    	chart3G.select(".x.axis")
 	        .transition()
 	        .duration(1000)
 	        .call(xAxis);
+	}
+
+	function updateYAxis() {
+    	chart3G.select(".y.axis")
+	        .transition()
+	        .duration(1000)
+	        .call(yAxis);
     }
 
     function updateDots() {
-        temp2.attr("cx", function(d) {
+        temp3.attr("cx", function(d) {
             	var score = 0;
-            	for (var i = 0; i < function_list.length; i++) {
-            		score += (d[function_list[i].id]-function_list[i].min)/(function_list[i].max-function_list[i].min)
+            	for (var i = 0; i < xfunction_list.length; i++) {
+            		score += (d[xfunction_list[i].id]-xfunction_list[i].min)/(xfunction_list[i].max-xfunction_list[i].min)
             	}
         		return xScale(score); 
+        	})
+        temp3.attr("cy", function(d) {
+            	var score = 0;
+            	for (var i = 0; i < yfunction_list.length; i++) {
+            		score += (d[yfunction_list[i].id]-yfunction_list[i].min)/(yfunction_list[i].max-yfunction_list[i].min)
+            	}
+        		return yScale(score); 
         	})
     }
 
